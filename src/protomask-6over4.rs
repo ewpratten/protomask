0d627d430b7a8324a1469ae31741831b2d27114e
Evan Pratten <ewpratten@gmail.com>
2023-08-03 12:18:09 -0400

    Use matrix for builds

--- src/protomask-6over4.rs
+++ src/protomask-6over4.rs
@@ -1,5 +1,5 @@
-
+//! This is a placeholder for a future 6over4 implementation
 #[tokio::main]
-pub async fn main(){
-    
+pub async fn main() {
+    println!("You did it! You found the incomplete binary :)");
 }

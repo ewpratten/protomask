a6f575c8d84fda3a01efcab5d7388aaab75c82c0
Evan Pratten <evan@ewpratten.com>
2024-02-12 09:54:16 -0500

    fix version order

--- scripts/cargo-publish-all.sh
+++ scripts/cargo-publish-all.sh
@@ -3,8 +3,8 @@ set -ex
 
 cargo publish --package easy-tun
 cargo publish --package fast-nat
-cargo publish --package interproto
 cargo publish --package protomask-metrics
+cargo publish --package interproto
 cargo publish --package rfc6052
 cargo publish --package rtnl
 cargo publish --package protomask

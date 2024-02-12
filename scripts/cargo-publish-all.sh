4b2ddf2a7350cd54470812bf55af5cc0f0ae017a
Evan Pratten <evan@ewpratten.com>
2024-02-12 09:55:34 -0500

    quiet fail on libs

--- scripts/cargo-publish-all.sh
+++ scripts/cargo-publish-all.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 set -ex
 
-cargo publish --package easy-tun
-cargo publish --package fast-nat
-cargo publish --package protomask-metrics
-cargo publish --package interproto
-cargo publish --package rfc6052
-cargo publish --package rtnl
+cargo publish --package easy-tun || true
+cargo publish --package fast-nat || true
+cargo publish --package protomask-metrics || true
+cargo publish --package interproto || true
+cargo publish --package rfc6052 || true
+cargo publish --package rtnl || true
 cargo publish --package protomask

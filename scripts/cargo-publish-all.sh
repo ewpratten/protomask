6542d66994c27139febc412c00530cb4da704f81
Evan Pratten <evan@ewpratten.com>
2024-02-12 09:52:27 -0500

    Add mega publish script

---
+++ scripts/cargo-publish-all.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+set -ex
+
+cargo publish --package easy-tun
+cargo publish --package fast-nat
+cargo publish --package interproto
+cargo publish --package protomask-metrics
+cargo publish --package rfc6052
+cargo publish --package rtnl
+cargo publish --package protomask
